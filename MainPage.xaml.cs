ada76bd63d82c62492e87750e4fc66b97cadc296
jasminlosbanos <jasminlosbanos@gmail.com>
2025-04-21 07:50:27 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Passenger
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void goToSignUpClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+    }
+
+}
