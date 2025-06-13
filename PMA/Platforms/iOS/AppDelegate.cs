f7bc7a46733b7a98cfedf8de00f339873833e098
Remy Dianne Ventura <164834503+rems404@users.noreply.github.com>
2025-06-13 18:52:42 +0800

    Add files via upload

---
+++ PMA/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PMA
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
